4ff5934b48bc223ffd254959bcef45661ac3bf33
dvru <damvaru4@gmail.com>
2020-02-18 14:45:42 -0600

    practice

--- data_types.rb
+++ data_types.rb
@@ -1,6 +1,14 @@
 # data types are types of data stored 
 # in ruby that can be used 
 
-# string is plain text in quotation marks
-name = "Mike"
-age = 75
+name = "Mike" # text form with a string: plain text in quotation marks
+
+age = 75 # number form with an integer, whole number
+
+gpa = 3.45454 # decimal numbers as floats, floating numbers
+
+isfemale = true # boolean true or false values
+istall = false
+
+flaws = nil # nil values, means it doesnt have value 
+
