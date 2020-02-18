6dfed4139c44dfccce77fbb841de0824e1bc13e4
dvru <damvaru4@gmail.com>
2020-02-18 15:00:06 -0600

    practice

---
+++ strings.rb
@@ -0,0 +1,44 @@
+
+#1 basics when using strings
+
+#2 methods are block of code to call, 
+# to modify, 
+# or give info about strings
+
+puts "first\" lane"
+# # prints first" lane
+
+puts "first\nlane"
+# print on two seperate lines
+
+# variable and value
+phrase = "first lane"
+puts phrase
+
+#2
+phrase = "first lane"
+puts phrase.upcase()
+# converts to uppercase
+
+phrase = "first lane"
+puts phrase.downcase()
+# converts to downcase
+
+phrase = "    first lane    "
+puts phrase.strip()
+# delete space in between string and text
+
+phrase = "first lane"
+puts phrase.length()
+# how many characters in between string
+
+phrase = "first lane"
+puts phrase.include? "lane"
+# asking the include method if the phrase includes the string added
+# which will result in true 
+
+phrase = "first lane"
+puts phrase.include? "last"
+# asking the include method if the phrase includes the string added
+# which will result in false 
+
