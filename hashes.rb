9eaf1f0d9e4d91f1c3a99ebae402e46c3b55b323
dvru <damvaru4@gmail.com>
2020-02-19 15:54:44 -0600

    practice

---
+++ hashes.rb
@@ -0,0 +1,21 @@
+# hashes are key,value pairs,
+# dictionary, name:definition
+
+#build hashes to store data
+
+states = {
+    "Pennsylvania" => "PA",
+    "New York" => "NY",
+    "Oregon" => "OR"
+}
+
+puts states["Oregon"] #prints OR
+# puts states["New York"] #prints NY
+
+# states = {
+#     :Pennsylvania => "PA",
+#     "New York" => "NY",
+#     "Oregon" => "OR"
+# }
+
+# puts states[:Pennsylvania] #prints PA
