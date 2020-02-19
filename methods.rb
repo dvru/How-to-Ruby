c71cfd7e4730bd03828c73b9b584a044a6a5854e
dvru <damvaru4@gmail.com>
2020-02-19 16:52:29 -0600

    Changes

--- methods.rb
+++ methods.rb
@@ -1,14 +1,14 @@
 #basics of using methods
 #how to write methods 
 
-# def sayhi()
-#     puts "Hello User"
-# end
+def sayhi()
+    puts "Hello User"
+end
 # #code will only run if called
 
-# puts "Top" #prints Top
-# sayhi #prints Hello User
-# puts "Bottom" #prints Bottom
+puts "Top" #prints Top
+sayhi #prints Hello User
+puts "Bottom" #prints Bottom
 
 def sayhi()
     puts "Hello User"
