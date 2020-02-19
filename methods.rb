bfb8949bfed7e62eb51f63a34a619558771a9d49
dvru <damvaru4@gmail.com>
2020-02-19 16:39:14 -0600

    methods

---
+++ methods.rb
@@ -0,0 +1,15 @@
+#basics of using methods
+#how to write methods 
+
+# def sayhi()
+#     puts "Hello User"
+# end
+# #code will only run if called
+
+# puts "Top" #prints Top
+# sayhi #prints Hello User
+# puts "Bottom" #prints Bottom
+
+def sayhi()
+    puts "Hello User"
+end
