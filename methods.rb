09452baee562535216a31d330e0dedf799abf593
dvru <damvaru4@gmail.com>
2020-02-24 13:29:29 -0600

    methods

--- methods.rb
+++ methods.rb
@@ -10,6 +10,9 @@ puts "Top" #prints Top
 sayhi #prints Hello User
 puts "Bottom" #prints Bottom
 
-def sayhi()
-    puts "Hello User"
+def sayhi(name, age)
+    puts ("Hello " + name + ", you are " + age.to_s)
 end
+# this method can accept params in parenthesis
+# always pass integers to a string to run using .to_s
+sayhi("Bob", 20)
