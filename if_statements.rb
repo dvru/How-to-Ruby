a74183632ab3ba0c0864e7180a24ce6d2c9cf17a
dvru <damvaru4@gmail.com>
2020-02-24 13:54:23 -0600

    if statments

---
+++ if_statements.rb
@@ -0,0 +1,43 @@
+# if statements are structures to help the program make decisions
+# respond to different info in the program
+# makes the program smarter
+
+# I wake up 
+# If I'm hungry #condition is true/fale if Im hungry 
+#     I eat breakfast #if true then you eat breakfast, if false 
+#                     #then move to next line
+
+# I leave my house 
+# if it's cloudy # true
+#     I bring an umbrella #then
+# otherwise #false
+#     I bring sunglasses #then
+
+# Im at a restaurant 
+# if I want veggies
+#     I order cauliflower
+# otherwise if I want pasta
+#     I order spaghetti & mushrooms
+# otherwise 
+#     I order a salad.
+
+# a boolean variable, isfemale is condition
+isfemale = false
+issmall = true
+# if true it will print, if false it wont and can move on to next line
+if isfemale and issmall
+    puts "You are small female" #true
+elsif isfemale and !issmall
+    puts "You are a tall female"
+elsif !isfemale and issmall
+    puts " You are not female and are small"
+else 
+    puts "You are not female" #false
+end
+
+# use and, or to add multiple conditions.
+# using and, will execute both
+# using or, will execute one
+
+# ! before anything, to show its not something, 
+# the negation operator
