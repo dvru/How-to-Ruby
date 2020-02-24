893b0cfef2525fab1b460689739081b862c4e066
dvru <damvaru4@gmail.com>
2020-02-24 13:37:33 -0600

    treturn statments

---
+++ return_statements.rb
@@ -0,0 +1,17 @@
+# using returns in ruby methdods, 
+# calling methods will give multiple info back
+
+def cube(num) #passing in one number
+return num * num * num, 50 
+     
+end
+
+puts cube(2) #prints 8
+puts cube(3) #prints 27
+puts cube(3) [1] 
+# prints the cude method and index
+
+# any code after return statement wont print
+
+# return num * num * num, "hello" 
+# will return hello after every cube method
