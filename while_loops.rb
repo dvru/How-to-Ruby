2b7596bf5c5ee70b45c41154ea671dc5ddb9cca8
dvru <damvaru4@gmail.com>
2020-02-24 16:26:27 -0600

    While loops

---
+++ while_loops.rb
@@ -0,0 +1,8 @@
+# while loops is a structure that allows you to loop through a block of code a significant amount of times
+# all the code iside while and end will keep getting execute as long as that condition is true
+index = 1 
+while index <=5
+    puts index #print index
+    index += 1 #incrementing index
+end
+
