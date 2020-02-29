691351c035ff3d6839319eb2012ecc07f557eeba
dvru <damvaru4@gmail.com>
2020-02-29 11:33:39 -0600

    comments

---
+++ comments.rb
@@ -0,0 +1,8 @@
+# comment is a line of code that ruby will ignore
+# notes in files, such as this.
+# add code before or in line or after code
+
+# This line prints text
+puts "Comments are fun" # Comment
+
+# puts "Comments are fun" comments out code to temp disable code
